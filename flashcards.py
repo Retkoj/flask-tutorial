18d3671d88666b76cd100017a8b8061e42323c13
Retkoj <jokekalter@gmail.com>
2022-12-28 12:11:53 +0100

    linking from home to cards and cards to home. Possible to walk through cards

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 from dummy_db import db
 
@@ -9,11 +9,16 @@ viewed_counter = 0
 @app.route("/")
 def welcome():
     return render_template("welcome.html",
-                           message="Message from view function")
+                           cards=db)
 
 
-@app.route("/card")
-def card_view():
-    card = db[0]
-    return render_template("card.html",
-                           card=card)
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html",
+                               card=card,
+                               index=index,
+                               max_index=len(db) - 1)
+    except IndexError:
+        abort(404)
