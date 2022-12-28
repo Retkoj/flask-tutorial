00102fd5303591faad28a5d4a3175b172d71742f
Retkoj <jokekalter@gmail.com>
2022-12-28 10:36:13 +0100

    Jinja template and view function with variable

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,4 @@
-import datetime
-
-from flask import  Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 viewed_counter = 0
@@ -8,16 +6,5 @@ viewed_counter = 0
 
 @app.route("/")
 def welcome():
-    return "Welcome to flask flashcards!"
-
-
-@app.route("/date")
-def date():
-    return f"Page served at {datetime.datetime.now()}"
-
-
-@app.route("/viewed")
-def viewed():
-    global viewed_counter
-    viewed_counter += 1
-    return f"Page has been viewed {viewed_counter}"
+    return render_template("welcome.html",
+                           message="Message from view function")
