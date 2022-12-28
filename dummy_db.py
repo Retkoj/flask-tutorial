77fa7fb4288f1a1f60085a6a1e05ffd67156669d
Retkoj <jokekalter@gmail.com>
2022-12-28 16:06:24 +0100

    Create and remove cards, plus some extra buttons

--- dummy_db.py
+++ dummy_db.py
@@ -1,14 +1,36 @@
 import json
 
 
-def load_db():
-    """
-    NOTE: not suitable for production environment
+class FlashCardDB:
+    def __init__(self):
+        self.db = self.load_db()
 
-    :return:
-    """
-    with open("data/flashcards_db.json") as file:
-        return json.load(file)
+    def load_db(self):
+        """
+        NOTE: not suitable for production environment
+
+        :return:
+        """
+        with open("data/flashcards_db.json") as file:
+            return json.load(file)
+
+    def get_db(self):
+        return self.db
+
+    def get_card_by_index(self, index):
+        return self.db[index]
+
+    def write_db(self):
+        with open("data/flashcards_db.json", "w") as file:
+            json.dump(self.db, file)
+
+    def add_to_db(self, entry):
+        self.db.append(entry)
+        self.write_db()
+
+    def remove_from_db(self, index):
+        self.db.pop(index)
+        self.write_db()
 
 
-db = load_db()
+db_handler = FlashCardDB()
--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, abort, jsonify
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
 
-from dummy_db import db
+from dummy_db import db_handler
 
 app = Flask(__name__)
 viewed_counter = 0
@@ -13,7 +13,7 @@ def welcome():
     to show a list of cards.
     """
     return render_template("welcome.html",
-                           cards=db)
+                           cards=db_handler.get_db())
 
 
 @app.route("/card/<int:index>")
@@ -24,25 +24,55 @@ def card_view(index):
     If index not found, returns a 404.
     """
     try:
-        card = db[index]
+        card = db_handler.get_card_by_index(index)
         return render_template("card.html",
                                card=card,
                                index=index,
-                               max_index=len(db) - 1)
+                               max_index=len(db_handler.get_db()) - 1)
     except IndexError:
         abort(404)
+
+
+@app.route("/add_card", methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        card = {"question": request.form["question"],
+                "answer": request.form["answer"]}
+        db_handler.add_to_db(card)
+        return redirect(url_for("card_view", index=len(db_handler.get_db()) - 1))
+    elif request.method == "GET":
+        return render_template("add_card.html")
+
+
+@app.route("/remove_card/<int:index>", methods=["GET", "POST"])
+def remove_card(index):
+    print("call")
+    if request.method == "POST":
+        print("call post")
+        db_handler.remove_from_db(index)
+        return redirect(url_for("welcome",
+                                cards=db_handler.get_db()))
+    elif request.method == "GET":
+        print("call get")
+        try:
+            card = db_handler.get_card_by_index(index)
+            return render_template("remove_card.html",
+                                   card=card,
+                                   index=index)
+        except IndexError:
+            abort(404)
 
 
 @app.route("/api/card/")
 def api_card_list():
     """Return a list of all cards"""
-    return jsonify(db)
+    return jsonify(db_handler.get_db())
 
 
-@ app.route('/api/card/<int:index>')
+@app.route('/api/card/<int:index>')
 def api_card_detail(index):
     """Return a single card by index"""
     try:
-        return db[index]
+        return db_handler.get_card_by_index(index)
     except IndexError:
         abort(404)
