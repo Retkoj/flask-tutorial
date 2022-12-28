a4507d544edf0fa316f498c87f24077b45059eb2
Retkoj <jokekalter@gmail.com>
2022-12-28 10:23:26 +0100

    Added view counter and current datetime functions and routes

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,23 @@
+import datetime
+
 from flask import  Flask
 
 app = Flask(__name__)
+viewed_counter = 0
 
 
 @app.route("/")
 def welcome():
     return "Welcome to flask flashcards!"
+
+
+@app.route("/date")
+def date():
+    return f"Page served at {datetime.datetime.now()}"
+
+
+@app.route("/viewed")
+def viewed():
+    global viewed_counter
+    viewed_counter += 1
+    return f"Page has been viewed {viewed_counter}"
